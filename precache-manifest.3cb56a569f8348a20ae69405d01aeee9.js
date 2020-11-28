04e7604ee0d335669822c84a0297a3c2bb11562b
yogeshsharma4477 <yogeshwarsharma4477@gmail.com>
2020-11-28 19:20:57 +0530

    Updates

---
+++ precache-manifest.3cb56a569f8348a20ae69405d01aeee9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947011bed2c34c96028083031ae230a9",
+    "url": "/Robofriend/index.html"
+  },
+  {
+    "revision": "8c3f264a1db6651249e0",
+    "url": "/Robofriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "32ee3f536dfa8196d883",
+    "url": "/Robofriend/static/css/main.45cd11d7.chunk.css"
+  },
+  {
+    "revision": "8c3f264a1db6651249e0",
+    "url": "/Robofriend/static/js/2.a9ef5f63.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Robofriend/static/js/2.a9ef5f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ee3f536dfa8196d883",
+    "url": "/Robofriend/static/js/main.dde37461.chunk.js"
+  },
+  {
+    "revision": "aa332650baad8ad9bc36",
+    "url": "/Robofriend/static/js/runtime-main.df88e8ff.js"
+  }
+]);
